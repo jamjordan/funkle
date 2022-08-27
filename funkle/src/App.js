fe2f883d6482979229f3742459495537b71db843
jamjordan <102589018+jamjordan@users.noreply.github.com>
2022-08-27 14:10:28 +0800

    funkle 2.0

--- funkle/src/App.js
+++ funkle/src/App.js
@@ -1,15 +1,67 @@
-import { Routes, Route } from "react-router-dom";
+import PLAYER_DATA from "./player-data.json";
+
+import { useState } from "react";
+
 import Game from "./components/game/game.component";
-import Header from "./components/header/header.component";
 
 const App = () => {
+  const playerOptions = PLAYER_DATA.map((player, index) => {
+    return { value: index, text: player.Player };
+  });
+
+  const [answer, setAnswer] = useState(PLAYER_DATA[0])
+
+  const startGame = () =>
+  {
+    const newAnswer = PLAYER_DATA[Math.floor(Math.random() * PLAYER_DATA.length)];
+    setAnswer(newAnswer);
+    setResults([])
+
+  }
+
+  
+
+  const defaultOptions = [{ value: "", text: "--Choose an option--" }];
+
+  const options = defaultOptions.concat(playerOptions);
+
+  const [selected, setSelected] = useState(options[0].value);
+
+  const [results, setResults] = useState([]);
+
+  const handleChange = (event) => {
+    setSelected(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    const currentResults = results;
+    const newResults = currentResults.concat([selected]);
+    setResults(newResults);
+    console.log(newResults);
+    console.log(answer);
+  };
+
   return (
     <div>
-    <Routes>
-      <Route path="/" element={<Header />}>
-        <Route index element={<Game />} />
-      </ Route>
-    </Routes>
+      <h1> Funkle</h1>
+      <select value={selected} onChange={handleChange}>
+        {options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.text}
+          </option>
+        ))}
+      </select>
+      <button onClick={handleSubmit}>Submit</button>
+      {results.map((player) => {
+
+        return(
+          <Game guess = {PLAYER_DATA[player]} answer = {answer}/>
+        
+        
+        );
+      })}
+
+      <button onClick={startGame}>New Game</button>
     </div>
   );
 };
--- funkle/src/components/game/game.component.jsx
+++ funkle/src/components/game/game.component.jsx
@@ -1,9 +1,73 @@
-const Game = () => {
-    return(
-        <div>
-            <h1>Hello, this is the Game</h1>
+import { Fragment } from "react";
+import "./game.styles.scss";
+
+const Game = ({ guess, answer }) => {
+  if (guess.Player === answer.Player) {
+    return (
+      <div>
+        <h1>{guess.Player}</h1>
+        <h1>Correct</h1>
+      </div>
+    );
+  }
+
+  return (
+    <Fragment>
+    <div>
+      <h1 className="player-name">{guess.Player}</h1>
+      <div className="player-flex">
+        <div
+          className={`player-result ${guess.Team === answer.Team ? "yes" : "no"}`}
+        >
+          <p>Team:</p>
+          <p>{guess.Team}</p>
         </div>
-    )
-}
+        <div className={`player-result ${guess.Owners === answer.Owners ? "yes" : "no"}`}>
+          <p>Owners:</p>
+          <p>{guess.Owners}</p>
+          {guess.Owners > answer.Owners ? (<h6>lower</h6>) : ''}
+          {guess.Owners < answer.Owners ? (<h6>higher</h6>) : ''}
+        </div>
+
+        <div className={`player-result ${guess.MaxScore === answer.MaxScore ? "yes" : "no"}`}>
+          <p>Max Score:</p>
+          <p>{guess.MaxScore}</p>
+          {guess.MaxScore > answer.MaxScore ? (<h6>lower</h6>) : ''}
+          {guess.MaxScore < answer.MaxScore ? (<h6>higher</h6>) : ''}
+        </div>
+
+        <div className={`player-result ${guess.MinScore === answer.MinScore ? "yes" : "no"}`}>
+          <p>Min Score:</p>
+          <p>{guess.MinScore}</p>
+          {guess.MinScore > answer.MinScore ? (<h6>lower</h6>) : ''}
+          {guess.MinScore < answer.MinScore ? (<h6>higher</h6>) : ''}
+          
+        </div>
+
+        <div className={`player-result ${guess.Benched === answer.Benched ? "yes" : "no"}`}>
+          <p>Benched:</p>
+          <p>{guess.Benched}</p>
+          {guess.Benched > answer.Benched ? (<h6>lower</h6>) : ''}
+          {guess.Benched < answer.Benched ? (<h6>higher</h6>) : ''}
+        </div>
+
+        <div className={`player-result ${guess.Games === answer.Games ? "yes" : "no"}`}>
+          <p>Games:</p>
+          <p>{guess.Games}</p>
+          {guess.Games > answer.Games ? (<h6>lower</h6>) : ''}
+          {guess.Games < answer.Games ? (<h6>higher</h6>) : ''}
+        </div>
+
+        <div className={`player-result ${guess.Finals === answer.Finals ? "yes" : "no"}`}>
+          <p>Finals:</p>
+          <p>{guess.Finals}</p>
+          {guess.Finals > answer.Finals ? (<h6>lower</h6>) : ''}
+          {guess.Finals < answer.Finals ? (<h6>higher</h6>) : ''}
+        </div>
+      </div>
+    </div>
+    </Fragment>
+  );
+};
 
 export default Game;
